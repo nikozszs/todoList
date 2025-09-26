a3671efd37c14c57d9804688e0b9bda35e02748c
Nikozszs <06@yandex.ru>
2025-09-26 20:00:06 +0300

    add todoItem

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import TodoForm from '@/components/TodoForm/TodoForm';
 import styles from './App.module.css';
+import TodoItem from '@/components/TodoItem/TodoItem';
 
 export default function App() {
     return (
         <div className={styles.app}>
-            <h1 className={styles.text}>To do list</h1>
+            <h1 className={styles.title}>To do list</h1>
             <TodoForm />
         </div>
     )
---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,50 @@
+import { ITodo } from "@/types";
+import { useState } from "react";
+import './TodoItem.module.css';
+import { Checkbox, Input } from 'antd';
+
+interface TodoItemProps {
+    todo: ITodo;
+    onEdit: (id: string, newText: string) => void;
+    onDelete: (id: string) => void;
+    onToggle: (id: string) => void;
+}
+
+export default function TodoItem({todo, onEdit, onDelete, onToggle}: TodoItemProps) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editText, setEditText] = useState(todo.text);
+
+    const handleEdit = () => {
+        if (isEditing) {
+            const trimText = editText.trim();
+            if (trimText) {
+                onEdit(todo.id, trimText)
+            } else {
+                setEditText(todo.text);
+            }
+        }
+        setIsEditing(!isEditing);
+    }
+
+    const handleSave = () => {
+        handleEdit();
+    }
+
+    const handleCancel = () => {
+        setEditText(todo.text);
+        setIsEditing(false);
+    }
+
+    return (
+        <div className={`todo-item ${todo.completed ? 'completed': ''}`}>
+            <div className="todo-content">
+                <Checkbox checked={todo.completed} onChange={() => onToggle(todo.id)} />
+                {/* {isEditing ? (
+                    <div className="edit-container">
+                        <Input type="text" value={editText} onChange={(e) => setEditText(e.target.value)}/>
+                    </div>
+                ) : ()} */}
+            </div>
+        </div>
+    )
+}
